31d58454443e4620be0cb4077b29ac3db2943805
taja01 <jarvas.tamas@gmail.com>
2025-06-09 14:01:09 +0200

    one more stupid test

--- Concurrency/Concurrency.UnitTests/EmailLockServiceTests.cs
+++ Concurrency/Concurrency.UnitTests/EmailLockServiceTests.cs
@@ -28,5 +28,25 @@ namespace Concurrency.UnitTests
             await _service.ReleaseEmailAsync(email1);
             await _service.ReleaseEmailAsync(email2);
         }
+
+        [Test]
+        public async Task Test2()
+        {
+            var acquireTimeout = TimeSpan.FromSeconds(1);
+            var email1 = await _service.TryAcquireEmailAsync(acquireTimeout);
+            var email2 = await _service.TryAcquireEmailAsync(acquireTimeout);
+
+            Assert.That(email1, Is.Not.Null);
+            Assert.That(email2, Is.Not.Null);
+
+            await _service.ReleaseEmailAsync(email1);
+            await _service.ReleaseEmailAsync(email2);
+
+            var email3 = await _service.TryAcquireEmailAsync(acquireTimeout);
+
+            Assert.That(email3, Is.Not.Null);
+
+            await _service.ReleaseEmailAsync(email3);
+        }
     }
 }
